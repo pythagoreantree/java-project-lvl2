335e1b4f63922be2f95f4ac2831a1b9e94506b39
Maria Teplinskaya <pythagoreantree@googlemail.com>
2022-07-25 17:58:55 +0300

    + Step 6-7: linter errors in tests are fixed

--- src/test/java/hexlet/code/DifferTest.java
+++ src/test/java/hexlet/code/DifferTest.java
@@ -3,7 +3,6 @@ package hexlet.code;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
